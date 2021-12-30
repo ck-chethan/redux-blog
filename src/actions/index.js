715e845e365ac49d6ce01b5b62fa79e1aaf3d4f6
chethanck <chethanckm66@gmail.com>
2021-12-30 16:01:55 +0530

    Memoization issue

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async distpatch => {
@@ -9,10 +10,21 @@ export const fetchPosts = () => async distpatch => {
     });
 };
 
-export const fetchUser = (id) => async dispatch =>{
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch ({
-        type: "FETCH_USER",
-        payload: response.data
+// export const fetchUser = (id) => async dispatch =>{
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch ({
+//         type: "FETCH_USER",
+//         payload: response.data
+//     });
+// }
+
+
+export const fetchUser = function(id) {
+    return _.memoize(async function(dispatch) {
+        const response = await jsonPlaceholder.get(`/users/${id}`);
+        dispatch ({
+            type: "FETCH_USER",
+            payload: response.data
+        });
     });
-}
+};
